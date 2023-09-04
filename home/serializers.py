e2e1729e83bbd771273e5b3b8dd6679a6eb098a8
greco <grecoalonzopolinga@gmail.com>
2023-09-04 17:37:59 +0800

    Modified serializers to get all fields

--- home/serializers.py
+++ home/serializers.py
@@ -4,9 +4,9 @@ from .models import ProcessedData, RawData
 class RawDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = RawData
-        fields = ('Node_ID','Ultrasonic_CM','MQ2_ppm', 'MQ3_ppm', 'MQ6_ppm', 'Flame_Data', 'Weight_lbs')
+        fields = '__all__' 
         
 class ProcessedDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProcessedData
-        fields = ('node_id', 'fill_level','mttc','mq2_ppm', 'mq3_ppm', 'mq6_ppm', 'mq2_change', 'mq3_change', 'mq6_change', 'timestamp')
+        fields = '__all__' 
