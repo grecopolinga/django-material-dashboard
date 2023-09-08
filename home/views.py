216186ca90de6aff24e28c25f687089b5d0e77ea
mjplagman <mjplagman16@gmail.com>
2023-09-08 14:37:09 +0800

    fill level graph done.

--- home/views.py
+++ home/views.py
@@ -19,10 +19,10 @@ import serial
 import threading
 import pytz
 
-Node1 = {"Node_ID": 0, "Ultrasonic_CM":0, "Ultrasonic_Inch":0, "MQ2_Data":0, "MQ3_Data":0, "MQ6_Data":0, "Flame_Data":0, "Weight_lbs":0}
-Node2 = {"Node_ID": 0, "Ultrasonic_CM":0, "Ultrasonic_Inch":0, "MQ2_Data":0, "MQ3_Data":0, "MQ6_Data":0, "Flame_Data":0, "Weight_lbs":0}
-Node3 = {"Node_ID": 0, "Ultrasonic_CM":0, "Ultrasonic_Inch":0, "MQ2_Data":0, "MQ3_Data":0, "MQ6_Data":0, "Flame_Data":0, "Weight_lbs":0}
-Node4 = {"Node_ID": 0, "Ultrasonic_CM":0, "Ultrasonic_Inch":0, "MQ2_Data":0, "MQ3_Data":0, "MQ6_Data":0, "Flame_Data":0, "Weight_lbs":0}
+Node1 = {"Node_ID": 1, "Ultrasonic_CM":0, "Ultrasonic_Inch":0, "MQ2_Data":0, "MQ3_Data":0, "MQ6_Data":0, "Flame_Data":0, "Weight_lbs":0}
+Node2 = {"Node_ID": 2, "Ultrasonic_CM":0, "Ultrasonic_Inch":0, "MQ2_Data":0, "MQ3_Data":0, "MQ6_Data":0, "Flame_Data":0, "Weight_lbs":0}
+Node3 = {"Node_ID": 3, "Ultrasonic_CM":0, "Ultrasonic_Inch":0, "MQ2_Data":0, "MQ3_Data":0, "MQ6_Data":0, "Flame_Data":0, "Weight_lbs":0}
+Node4 = {"Node_ID": 4, "Ultrasonic_CM":0, "Ultrasonic_Inch":0, "MQ2_Data":0, "MQ3_Data":0, "MQ6_Data":0, "Flame_Data":0, "Weight_lbs":0}
 
 
 running = True
@@ -220,10 +220,10 @@ def processing():
     processed_data = ProcessedData.objects.filter(timestamp__gte=start_of_day, timestamp__lt=end_of_day)
     # Extract the fill level and timestamp data for the filtered ProcessedData objects
     fill_levels = [data.fill_level for data in processed_data]
-    #bin_ids = [data.node_ID for data in processed_data]
+    bin_ids = [data.node_ID for data in processed_data]
     timestamps = [data.timestamp.astimezone(ph_tz).strftime('%H:%M') for data in processed_data]
     # Pass fill_levels and timestamps as JSON objects
-    #bin_ids_json = json.dumps(bin_ids)
+    bin_ids_json = json.dumps(bin_ids)
     fill_levels_json = json.dumps(fill_levels)
     timestamps_json = json.dumps(timestamps)
     context = {
@@ -243,7 +243,7 @@ def processing():
         'data': processed_data, 
         'timestamps': timestamps_json, 
         'fill_levels': fill_levels_json,
-        #'bin_ids': bin_ids_json,
+        'bin_ids': bin_ids_json,
 
         'Bin2_ID': int(Node2["Node_ID"]),
         'Bin2_Fill_Level': int(Bin2_Fill_Level),
